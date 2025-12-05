f1d8c4e3a11b1809e5cec6dadbabd68b8a8a7f79
LuckyOneDev <imokshin2003@gmail.com>
2025-12-05 17:19:41 +0300

    updated baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,11 +18,11 @@ const config: Config = {
   url: 'https://luckyonedev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/homo-dominus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'LuckyOneDev', // Usually your GitHub org/user name.
+  organizationName: 'luckyonedev', // Usually your GitHub org/user name.
   projectName: 'homo-dominus', // Usually your repo name.
   deploymentBranch: 'build',
   onBrokenLinks: 'throw',
